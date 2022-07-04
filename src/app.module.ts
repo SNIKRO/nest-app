2f7d8abe36c8149225533e874157dfea0ee0e855
SNIKR <snikr@bk.ru>
2022-07-04 08:16:24 +0600

    change sequalize ti typeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { SequelizeModule } from '@nestjs/sequelize';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.models';
 import { UsersModule } from './users/users.module';
 import { RolesModule } from './roles/roles.module';
@@ -12,15 +12,15 @@ import { UserRoles } from './roles/user-roles.models';
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
+    TypeOrmModule.forRoot({
+      type: 'postgres',
       host: process.env.POSTGRES_HOST,
       port: Number(process.env.POSTGRES_PORT),
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      models: [User, Role, UserRoles],
-      autoLoadModels: true,
+      entities: [User, Role, UserRoles],
+      autoLoadEntities: true,
     }),
     UsersModule,
     RolesModule,
